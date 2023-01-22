c6ea211b8b7261717ec0b7874b5eeed33593733f
e-sollier <etienne.sollier@gmail.com>
2023-01-22 12:15:46 +0100

    change launch script

--- run_pipeline.sh
+++ run_pipeline.sh
@@ -4,4 +4,4 @@ module load htslib/1.12
 module load sambamba/0.7.1
 module load gatk/4.2.0.0
 
-bsub -o lsf.o%J -W 10:00 -R "rusage[mem=16G]" "snakemake --configfile config.yaml --use-conda --cluster 'bsub -o lsf.o%J -W {params.runtime} -n {params.threads} -R \"rusage[mem={params.memory}]\"' --jobs 100 --latency-wait 60"
+bsub -o lsf.o%J -W 10:00 -R "rusage[mem=16G]" "snakemake --configfile $1 --use-conda --cluster 'bsub -o lsf.o%J -W {params.runtime} -n {params.threads} -R \"rusage[mem={params.memory}]\"' --jobs 100 --latency-wait 60"
